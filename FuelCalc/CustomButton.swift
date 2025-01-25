d5068892e44d3664a51c0445d8de387cf90b4e52
Nathaniel Bedggood <ngbedggood@gmail.com>
2025-01-25 17:25:50 -0600

    Custom button now has colour modifier

--- FuelCalc/CustomButton.swift
+++ FuelCalc/CustomButton.swift
@@ -8,22 +8,27 @@
 import SwiftUI
 
 struct CustomButton: ButtonStyle {
+    
+    let buttonColor: Color
+    
     func makeBody(configuration: Configuration) -> some View {
         ZStack {
             
             let offset: CGFloat = 5
             
             RoundedRectangle(cornerRadius: 6)
-                .foregroundColor(.buttonColorSecondary)
+                .foregroundColor(buttonColor)
+                .brightness(-0.3)
                 .offset(y: offset)
             
             RoundedRectangle(cornerRadius: 6)
-                .foregroundColor(.button)
+                .foregroundColor(buttonColor)
                 .offset(y: configuration.isPressed ? offset: 0)
             
             configuration.label
                 .offset(y: configuration.isPressed ? offset : 0)
                 .foregroundStyle(.white)
+                .fontWeight(.bold)
         }
     }
 }
@@ -34,5 +39,5 @@ struct CustomButton: ButtonStyle {
     }
     .foregroundColor(.white)
     .frame(width: 50, height: 50)
-    .buttonStyle(CustomButton())
+    .buttonStyle(CustomButton(buttonColor: .red))
 }
