d2ebfcbf379f125ccd4e0942ebc21c0f938270eb
Nathaniel Bedggood <ngbedggood@gmail.com>
2025-01-26 16:25:05 -0600

    Custom toggle switch - more obvious how to switch units

---
+++ FuelCalc/CustomToggle.swift
@@ -0,0 +1,57 @@
+//
+//  CustomToggle.swift
+//  FuelCalc
+//
+//  Created by Nathaniel Bedggood on 26/01/2025.
+//
+
+import SwiftUI
+
+struct CustomToggle: ToggleStyle {
+    
+    let firstLabel: String
+    let secondLabel: String
+    
+    func makeBody(configuration: Configuration) -> some View {
+        
+        RoundedRectangle(cornerRadius: 8)
+            .fill(.gray)//.brightness(-0.2)
+            .frame(width: 160, height: 36)
+            .offset(y:3)
+            .overlay {
+                RoundedRectangle(cornerRadius: 8)
+                    .fill(.gray)
+                    .frame(width: 160, height: 30)
+                    .offset(y: 4)
+                    .overlay {
+                        RoundedRectangle(cornerRadius: 8)
+                            .fill(.blue).brightness(-0.3)
+                            .frame(width: 80, height: 32)
+                            .offset(x: configuration.isOn ? -40 : 40, y: 5)
+                        
+                        RoundedRectangle(cornerRadius: 8)
+                            .fill(.blue)
+                            .frame(width: 80, height: 32)
+                            .offset(x: configuration.isOn ? -40 : 40)
+                        Text("\(firstLabel)")
+                            .offset(x: -40)
+                            .foregroundColor(configuration.isOn ? .white : .darkGray)
+                        Text("\(secondLabel)")
+                            .offset(x: 40)
+                            .foregroundColor(configuration.isOn ? .darkGray : .white)
+                    }
+                    .onTapGesture {
+                        withAnimation(.spring(duration: 0.2)) {
+                            configuration.isOn.toggle()
+                        }
+                    }
+            }
+            .fontWeight(.bold)
+    }
+}
+
+#Preview {
+    
+    Toggle("test", isOn: .constant(true))
+        .toggleStyle(CustomToggle(firstLabel: "Label 1", secondLabel: "Label 2"))
+}
