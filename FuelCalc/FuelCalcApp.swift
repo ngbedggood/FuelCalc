9e031f414d94424a28be0fcae830f5f09be64bcb
Nathaniel Bedggood <ngbedggood@gmail.com>
2025-01-26 12:45:02 -0600

    Persistant saved trips, metric/MPG button toggles past trips

--- FuelCalc/FuelCalcApp.swift
+++ FuelCalc/FuelCalcApp.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import SwiftData
 
 @main
 struct FuelCalcApp: App {
@@ -13,5 +14,6 @@ struct FuelCalcApp: App {
         WindowGroup {
             ContentView()
         }
+        .modelContainer(for: Trip.self)
     }
 }
